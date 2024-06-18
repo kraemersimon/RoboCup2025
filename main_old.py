71695aabde284c59cb0d326b4d7238f00136c5c3
Niklas Giesen <niklas-disc@giesen-hw.de>
2024-06-18 18:05:17 +0200

    Added a new file called main and renamed the old one to main_old. New file features: fixed FPS counter, better structure, Picamera2 library

--- main_old.py
+++ main_old.py
@@ -1,65 +1,29 @@
-import cv2
-from picamera2 import Picamera2
-import time
-import numpy as np
-import RPi.GPIO as GPIO
 import serial
+import time
+import cv2
+import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import RPi.GPIO as GPIO 
 
-
-#Settings
-ResX, ResY = 640, 480 #Set resolution here
-FPS = 200 #Set a FPS cap here (Note that it caps the FPS)
-Debug = True
-Show_Fps = True
-BASE_SPEED = 80
-SENSITIVITY = 3
-
-#Camera stuff
-picam = Picamera2(0)
-#webcam = Picamera2(1)
-config = picam.create_video_configuration(main={"size": (ResX, ResY), "format": "RGB888"}, raw={"size": (640, 480)})
-picam.configure(config)
-picam.set_controls({"FrameRate": FPS})
-
-#GPIO stuff
+debug = True
 PIN_RST_BUTTON = 10
 PIN_OBSTACLE_BUTTON = 22
-GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(PIN_RST_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(PIN_OBSTACLE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-LEFT_MOTOR = 0
-RIGHT_MOTOR = 1
 
-#Arduino stuff
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 time.sleep(2) # wait for Nano to reboot
 
-#FPS counter variables
-frame_count = 0
-start_time = time.time()
+GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
+GPIO.setup(PIN_RST_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIN_OBSTACLE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-#Resizing Windows (for better performance)
-if Debug:    
-    cv2.namedWindow("Image from camera", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Image from camera", ResX, ResY)
-    cv2.namedWindow("Blackline", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Blackline", ResX, ResY)
-    cv2.namedWindow("Greensign", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Greensign", ResX, ResY)
-    cv2.namedWindow("RedLine", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("RedLine", ResX, ResY)
+LEFT_MOTOR = 0
+RIGHT_MOTOR = 1
 
-#Stuff for the programm
-kernel = np.ones((3, 3), np.uint8)
-Green_ignored = False
-Left_dose = False
-green_points = []
-new_sec = False
-sec_track = time.time()
-w_blk_old = 0
-picam.start()
+# linefollowing paramters
+BASE_SPEED = 80
+SENSITIVITY = 3
 
-#Some functions
 def send_motor_command(motor, direction, speed):
     speed = min(speed, 255)
     speed = speed // 4
@@ -82,17 +46,62 @@ def m(left, right, duration):
 
 def button_rst_pressed():
     return GPIO.input(PIN_RST_BUTTON)
-
 def button_obstacle_pressed():
     return GPIO.input(PIN_OBSTACLE_BUTTON)
-
-#Main Loop
-while True:
-    Img_Cam = picam.capture_array()
     
-    Blackline = cv2.inRange(Img_Cam, (0, 0, 0), (230, 80, 100))
-    Greensign = cv2.inRange(Img_Cam, (0, 80, 0), (200, 255, 80))
-    RedLine = cv2.inRange(Img_Cam, (0, 0, 65), (50, 50, 200))
+camera = PiCamera()
+camera.resolution = (160, 96)
+camera.rotation = 0
+camera.contrast = 70
+camera.brightness = 60
+
+#camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(160, 96))
+start_time = time.time()
+number_of_frames = 0 # frame counter for FPS
+w_red = 0
+w_blk = 0
+w_blk_old = 0
+Temp_frames = 0
+counter = 0
+countergreen = 0
+Left_dose = True
+Green_ignored = False
+counterdoublegreen = 0
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):    
+    
+    if button_rst_pressed():
+        m(0, 0, 0)
+        time.sleep(3)
+        while not button_rst_pressed():
+            pass
+        time.sleep(3)
+       
+    if button_obstacle_pressed():
+        if Left_dose:
+            m(-100, -100, 450)
+            m(-255, 255, 300)
+            m(160, 60, 3600)
+            m(-255, 255, 360)
+            m(-100, -100, 600)
+            Left_dose = False
+
+        elif Left_dose == False:
+            m(-100, -100, 450)
+            m(255, -255, 300)
+            m(60, 160, 3500)
+            m(255, -255, 360)
+            m(-100, -100, 600)
+            Left_dose = True
+    
+    original_image = frame.array
+    roi = original_image[0:75, 0:160]    
+    kernel = np.ones((3, 3), np.uint8)
+    roi = cv2.GaussianBlur(roi, ((9, 9)), 3, 3)
+   
+    Blackline = cv2.inRange(roi, (0, 0, 0), (230, 80, 100))
+    Greensign = cv2.inRange(roi, (0, 80, 0), (200, 255, 80))
+    RedLine = cv2.inRange(roi, (0, 0, 65), (50, 50, 200))
     
     Blackline = cv2.erode(Blackline, kernel, iterations = 2)
     Blackline = cv2.dilate(Blackline, kernel, iterations = 2)
@@ -111,7 +120,6 @@ while True:
     contours_grn_len = len(contours_grn)
     contours_red_len = len(contours_red)
     
-    #Blackline
     if contours_blk_len > 0:
         x_blk, y_blk, w_blk, h_blk = cv2.boundingRect(contours_blk[0])
         centerx_blk = x_blk + (w_blk / 2)
@@ -119,54 +127,69 @@ while True:
         (x_min, y_min), (w_min, h_min), ang = blackbox
         x_last = x_min
         y_last = y_min
-        setpoint = ResX / 2
+        if ang < -45 :
+            ang = 90 + ang
+        if w_min < h_min and ang > 0:    
+            ang = (90 - ang) * -1
+        if w_min > h_min and ang < 0:
+            ang = 90 + ang    
+        setpoint = 80
         error = int(x_min - setpoint)
+        ang = int(ang)
 
         box = cv2.boxPoints(blackbox)
         box = np.int0(box)
-        cv2.drawContours(Img_Cam, [box], 0, (0, 0, 255), 3)
-        cv2.line(Img_Cam, (int(x_min), 200), (int(x_min), 250), (255, 0, 0), 3)
-        
+        cv2.drawContours(roi, [box], 0, (0, 0, 255), 3)    
+        number_of_frames = number_of_frames + 1
+        cv2.line(roi, (int(x_min), 200), (int(x_min), 250), (255, 0, 0), 3)
+
+        # set motors
         m(BASE_SPEED + (SENSITIVITY * error), BASE_SPEED - (SENSITIVITY * error), 0)
-        
-    #Red Line    
+
+    
     if contours_red_len > 0:
         for i in range(contours_red_len):
             x_red, y_red, w_red, h_red = cv2.boundingRect(contours_red[i])
-            cv2.rectangle(Img_Cam, (x_red, y_red), (x_red + w_red, y_red + h_red), (0, 255, 0), 2)
-            
+            cv2.rectangle(roi, (x_red, y_red), (x_red + w_red, y_red + h_red), (0, 255, 0), 2)
+        
         if w_red > 150:
             m(255, 255, 150)
             m(0, 0, 0)
-            print("Red detected, sleeping...")
             time.sleep(7)
             
+    green_points = []
     
-    #All Turns
-    if w_blk < (ResX - 10) and w_blk > 60 and not Green_ignored:
+
+    if w_blk < 150 and w_blk > 60 and Green_ignored == False:
         countergreen = 0
         Green_ignored = True
         if h_blk > 70: 
-            print("Boosting: ")
             m(255, 255, 150)
-            Green_ignored = False
-    
 
-    if not Green_ignored:
+    
+    if countergreen >= 40:
+        Green_ignored = False
+
+    if contours_grn_len > 0 and counterdoublegreen <= 0 and Green_ignored == False:
+        counterdoublegreen = 20
+        Green_ignored = True
+
+    elif counterdoublegreen == 0:
+        Green_ignored = False
+
+    if Green_ignored == False:
         if contours_grn_len > 1:
             print("Turn 180 degrees")
             m(255, -255, 900)
             m(0, 0, 0)
 
         elif contours_grn_len > 0:
-            m(255, 255, 50)
-            m(0, 0, 0)
             for i in range(contours_grn_len):
                 x_grn, y_grn, w_grn, h_grn = cv2.boundingRect(contours_grn[i])
                 centerx_grn = x_grn + (w_grn / 2)
+                #centery_grn = y_grn + (h_grn / 2)
                 green_points.append(centerx_grn)
-                cv2.rectangle(Img_Cam, (x_grn, y_grn), (x_grn + w_grn, y_grn + h_grn), (255, 0, 0), 2)
-                
+                cv2.rectangle(roi, (x_grn, y_grn), (x_grn + w_grn, y_grn + h_grn), (255, 0, 0), 2)
         elif w_blk > 150:
             print("Go straight")
             m(255, 255, 500)
@@ -179,18 +202,28 @@ while True:
                 m(255, -255, 400)
                 m(255, 255, 100)
                 m(0, 0, 0)
-                green_points = []
-                
             elif all(point < centerx_blk for point in green_points):
                 print("Left")
                 m(255, 240, 375)
                 m(-255, 255, 350)
                 m(255, 255, 100)
                 m(0, 0, 0)
-                green_points = []
 
+    if debug == True:        
+        cv2.imshow("orginal_image", original_image) 
+        cv2.imshow("modified_image", roi) 
+        cv2.imshow("black", Blackline)       
+        cv2.imshow("green", Greensign)
+        cv2.imshow("red", RedLine)
+        print("Schwarz_Breite:", w_blk, "Rot_Breite:", w_red)
+        print("Ignore Green:", Green_ignored)
+        print("FPS:", str(int(number_of_frames / (time.time() - start_time))), "Error:", error)
 
-    if new_sec:
+    counter = counter + 1
+    countergreen = countergreen + 1
+    counterdoublegreen = counterdoublegreen - 1
+
+    if counter == 100:
         if w_blk_old > w_blk * 0.95 and w_blk_old < w_blk * 1.05:
             m(-255, 255, 100)
             m(255, -255, 100)
@@ -199,37 +232,16 @@ while True:
         else:
             w_blk_old = w_blk
 
-    else:
+    elif counter == 140:
         SENSITIVITY = 3
         BASE_SPEED = 80
-
-
-
-    if Debug:
-        cv2.imshow("Image from camera", Img_Cam)
-        cv2.imshow("Blackline", Blackline)
-        cv2.imshow("Greensign", Greensign)
-        cv2.imshow("RedLine", RedLine)
-
-    if Show_Fps:
-        frame_count += 1
-        if (time.time() - start_time) > 1:
-            fps = frame_count / (time.time() - start_time)
-            print(Img_Cam.shape, "at", round(fps), "fps")
-            frame_count = 0
-            start_time = time.time() 
-
-
-    #Timer
-    #1 sec
-    if (time.time() - sec_track) > 1:
-        new_sec = True
-        sec_track = time.time()   
-    else:
-        new_sec = False
+        counter = 0
         
-
-    if cv2.waitKey(1) == ord("q"):
-        picam.close()
+        
+    rawCapture.truncate(0)    
+    key = cv2.waitKey(1) & 0xFF    
+    if key == ord("q"):
+        m(0, 0, 0)
         cv2.destroyAllWindows()
+        ser.close()
         break
